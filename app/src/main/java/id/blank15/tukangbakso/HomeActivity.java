f6bb33749a6cafcde72fca865d0b236fee586ee6
blank15 <yudistiro6@gmail.com>
2018-04-07 09:26:11 +0700

    all

---
+++ app/src/main/java/id/blank15/tukangbakso/HomeActivity.java
@@ -0,0 +1,15 @@
+package id.blank15.tukangbakso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+      }
+}
